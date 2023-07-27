cf3a0606d681e02de8b27ae2e62bd0be7cb46599
FarrelHI <farrel.indallah@gmail.com>
2023-07-27 14:11:09 +0700

    makes the web functionable
    but still has some bugs and reset button hasn't been added it's function yet

--- script.js
+++ script.js
@@ -2,32 +2,13 @@ let bill = 0,
     numOfPeople = 0,
     tip = 0;
 
-
-function checkCustomInput() {
-    let checkBill = document.getElementById("bill").value;
-    let checkCustomTip = document.getElementById("custom-tip").value;
-    let checkPeople = document.getElementById("num-people").value;
-
-    bill = parseFloat(checkBill);
-    tip = parseFloat(checkCustomTip);
-    numOfPeople = parseFloat(checkPeople);
-
-    if (checkBill != "" && checkCustomTip != "" && checkPeople != ""){
-        calculateTip();
-    }
-
-    console.log(`bill:${checkBill} type: ${typeof checkBill}\nCustom Tip:${checkCustomTip} type: ${typeof checkCustomTip}\nPeople:${checkPeople} type: ${typeof checkPeople}`)
-}
-
-setInterval(checkCustomInput,500);
-
 function assignEvent(value) {
     let numOnly = value.replace(new RegExp("%", "g"), "");
     tip = parseFloat(numOnly);
     console.log(`tip: ${tip}, tip type: ${typeof tip}`)
 }
 
-const btn = document.querySelectorAll('button');
+const btn = document.querySelectorAll('button.tip-item');
 for (let i = 0;i<btn.length;i++){
     btn[i].addEventListener("click", function(){
         let clickValue = btn[i].innerHTML;
@@ -41,21 +22,53 @@ function assignResult(total,amount) {
 }
 
 function calculateTip() {
-    totalTip = bill*tip;
-    tipAmount = total/numOfPeople;
+    totalTip = (bill*tip)/100;
+
+    console.log(`\n Total Tip: ${totalTip} type of ${typeof totalTip}`);
+
+    tipAmount = totalTip/numOfPeople;
+    console.log(`\n People: ${numOfPeople} type of ${typeof numOfPeople}`);
+
+    console.log(`\n Tip Amount: ${tipAmount} type of ${typeof tipAmount}`);
+
 
     assignResult(totalTip,tipAmount);
 }
 
-function checkAllVariable() {
-    console.log(`bill: ${bill}`);
-    console.log(`people: ${numOfPeople}`);
-    console.log(`tip: ${tip}`);
+function checkCustomInput() {
+    let checkBill = document.getElementById("bill").value;
+    let checkCustomTip = document.getElementById("custom-tip").value;
+    let checkPeople = document.getElementById("num-people").value;
 
-    if (bill != 0 && numOfPeople != 0 && tip !=0){
-        console.log("koko");
-        calculateTip();
+    if (tip!=0 && checkCustomTip == ""){ //If only click button
+        console.log(tip);
+        checkCustomTip = tip;
     }
+    else if(tip!=0 && checkCustomTip !=""){ //If Input Custom after click Button
+        tip = parseFloat(checkCustomTip);
+
+    }
+    //Input Custom > Click Button Scenario hasn't been fix yet
+
+    console.log(`bill:${checkBill} type: ${typeof checkBill}\nCustom Tip:${checkCustomTip} type: ${typeof checkCustomTip}\nPeople:${checkPeople} type: ${typeof checkPeople}`)
+
+
+    if (checkBill != "" && checkCustomTip != "" && checkPeople != ""){
+        bill = parseFloat(checkBill);
+        numOfPeople = parseFloat(checkPeople);
+        console.log(tip);
+        console.log(bill);
+        console.log(numOfPeople);
+    
+    
+        if (bill != "" && tip != "" && numOfPeople != ""){
+            calculateTip();
+        }
+    }
+
+   
+
 }
 
-// setInterval(checkAllVariable,500);
+setInterval(checkCustomInput,500);
+
